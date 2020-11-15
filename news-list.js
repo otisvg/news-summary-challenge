d0e97857c4751aa0a23cfe25093cc68462d54798
Otis <otisvickersgraver@gmail.com>
2020-11-15 14:13:52 +0000

    headlines displaying on page, linked to articles

--- news-list.js
+++ news-list.js
@@ -1,42 +1,29 @@
 
-
-// let request = new XMLHttpRequest();
-// request.open("GET", "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/politics");
-// request.send();
-// request.onload = () => {
-//     let data = request.response;
-//     console.log(res);
-//     JSON.parse(res).results
-// }
-
-
-//   var request = new XMLHttpRequest();
-
-
-//   xmlhttp.onreadystatechange = function () {
-//       var data = xmlhttp.responseText;
- 
-// //   };
-
-// function loadDoc() {
-//   var xhttp = new XMLHttpRequest();
-//   xhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//       return this.responseText;
-//     }
-//   };
-//   xhttp.open("GET", "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/politics", true);
-//   xhttp.send();
-// }
-
-// console.log(loadDoc())
-
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    document.getElementById("demo").innerHTML =
-    this.getResponseHeader("Last-Modified");
+function fetchData() {
+  fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/politics?q=&show-fields=starRating,headline,thumbnail,short-url&show-refinements=all&order-by=relevance")
+  .then(response => {    
+      if (!response.ok){
+      throw Error("Error")
   }
-};
-xhttp.open("GET", "ajax_info.txt", true);
-xhttp.send();
+      return response.json();
+  })
+  .then(data => {
+    console.log(data);
+     const articles = data.response.results
+     .map(article => {
+      return `
+         <div class="news">
+         <p><img src="${article.webUrl}" alt=${article.webTitle}" /></p>
+          <h3>Headline: ${article.webTitle}</h3>
+          <p><a href=${article.webUrl} target="${article.webUrl}">Full Story:</a></p>
+         </div>
+         `
+     })
+     .join('');
+     document.querySelector('#app').insertAdjacentHTML('afterbegin', articles) 
+  }).catch(error => {   
+      console.log(error)
+  });
+}
+
+fetchData();
